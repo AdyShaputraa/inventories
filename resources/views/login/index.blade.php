656640c8db80c20ec3546d0acc8b0fe53dc6f00a
Ady Shaputra <Adyshaputra78@gmail.com>
2023-07-23 17:50:31 +0700

    fix bug login alert

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -25,19 +25,15 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>
             @endif
-            
-            @if (\Session::has('fail'))
-              <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{ session('fail') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-            @endif  
-              
-            @if (\Session::has('error'))
-              <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{ session('error') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
+
+            @if ($errors->any())
+              <br>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                  @foreach ($errors->all() as $error)
+                  {{ $error }}
+                  @endforeach
+                </div>
+              <br>
             @endif
 
             <form action="/login" method="post"> 
